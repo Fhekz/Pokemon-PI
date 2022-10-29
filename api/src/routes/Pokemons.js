e4178c3bd67b6e5e75544617d9f2c2f790552d5b
fhekz <fm.aldeco@gmail.com>
2022-10-29 18:49:16 -0300

    Commit Backend & Database

---
+++ api/src/routes/Pokemons.js
@@ -0,0 +1,62 @@
+const { Router } = require('express')
+const router = Router()
+const getAllPokemons = require('../controller/AllPokemons')
+const { Pokemon, Type } = require('../db')
+
+
+router.get('/', async (req, res) => {
+    const name = req.query.name;
+    const allPokemons = await getAllPokemons();
+    if(name){
+        let pokemonName = await allPokemons.filter(p => p.name.toLowerCase().includes(name.toLowerCase()))
+        pokemonName.length ? res.status(200).send(pokemonName) : res.status(404).send('404 Not Found')
+    }else{
+        res.status(200).send(allPokemons)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const allPokemons = await getAllPokemons();
+    if(id){
+        let pokemonId = await allPokemons.filter(p => p.id.toString() === id.toString())
+        pokemonId.length ? res.status(200).send(pokemonId) : res.status(404).send('404 Not Found')
+    }
+    else{
+        res.status(200).send(allPokemons)
+    }
+})
+
+router.post('/', async (req, res) => {
+    let {
+        name, 
+        hp, 
+        attack, 
+        defense, 
+        speed, 
+        height, 
+        weight, 
+        image, 
+        types
+    } = req.body;
+
+    const newPokemon = await Pokemon.create({
+        name,
+        hp,
+        attack,
+        defense,
+        speed,
+        height,
+        weight,
+        image
+    });
+
+    const typesDb = await Type.findAll({
+        where: { name: types}
+    })
+
+    newPokemon.addType(typesDb)
+    res.status(200).send('Pokemon created')
+})
+
+module.exports = router;
